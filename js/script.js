04f4f2cdc10746d6374ca60b94181e8ca30c894c
albertoBarl <alberto.barletta00@gmail.com>
2022-12-13 16:14:23 +0100

    added img folder

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+let barlfc = [
+  {
+    nome: "Wayne",
+    ruolo: "Founder & CEO",
+    foto: "",
+  },
+  {
+    nome: "Michael",
+    ruolo: "Chief Editor",
+    foto: "",
+  },
+  {
+    nome: "Jesus",
+    ruolo: "Office Manager",
+    foto: "",
+  },
+  {
+    nome: "Jesus",
+    ruolo: "Social Media Manager",
+    foto: "",
+  },
+  {
+    nome: "Michael",
+    ruolo: "Developer",
+    foto: "",
+  },
+  {
+    nome: "Jesus",
+    ruolo: "Graphic Designer",
+    foto: "",
+  },
+];
